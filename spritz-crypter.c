527e1b6ef4931e10fecaa66ecd92ce02feb4f936
Re4son Protagonist <re4son@whitedome.com.au>
2015-06-22 11:39:24 +1000

    Update spritz-crypter.c

--- spritz-crypter.c
+++ spritz-crypter.c
@@ -5,7 +5,7 @@
 *			depending on the command line arguments
 *                       We are using the experimental SPRITZ cypher
 *
-*    Author   : Re4son re4son [ at ] whitedome.com.au
+*    Author   : Re4son <re4son [ at ] whitedome.com.au>
 *    Purpose  : to encrypt / decrypt&execute a shellcode
 *    Usage    : Encrypt shellcode:    ./spritz-crypter <password> -e
 *		  then copy and paste the shellcode as shellcode below
